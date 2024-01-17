e31e13befcee864807c7c9ffcac0433ef45c78f5
9909Y-Robotics <24battistar@s.mpsct.org>
2024-01-16 22:27:42 -0500

    Flywheel - epic win if fix, epic fail if not

--- src/subsystemFiles/flywheel.cpp
+++ src/subsystemFiles/flywheel.cpp
@@ -9,44 +9,14 @@ double gain = 0.01; //0.01;
 
 // Flywheel control functions
 void setFlywheel() {
-    // L1 forward, L2 disabled
-    // Spins at 10v without TBH
-    flywheel = 127.0 / 12.0 * 10.0 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+    while ( true ) {
+        // L1 forward, L2 disabled
+        // Spins at 10v without TBH
+        flywheel = 127.0 / 12.0 * 10.0 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+    }
 }
 
 void flywheelTBHLoopOPControl() {
-    // Get the current RPM of the flywheel
-    current_fw_rpm = flywheel.get_actual_velocity();
-    
-    // Calculate the error between the current RPM and the target RPM
-    error = target_fw_rpm - current_fw_rpm;
-
-    fw_output += gain * error;
-
-    if ( (error > 0 && prev_error < 0) || (error < 0 && prev_error > 0) ) {
-        fw_output = 0.5 * (fw_output + tbh);
-        tbh = fw_output;
-        prev_error = error;
-    }
-    
-    if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) ) {
-        // Set the flywheel motor speeds based on the fw_output voltage
-        flywheel.move_velocity(fw_output);
-    }
-    else if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2) ) {
-        flywheel.move_velocity(-1 * fw_output);
-    }
-    else {
-        current_fw_rpm = flywheel.get_actual_velocity();
-        fw_output = 0.0;
-        tbh = 0.0;
-        prev_error = 0.0;
-        flywheel.brake();
-    }
-    pros::lcd::set_text(1, std::to_string(current_fw_rpm));
-}
-
-void flywheelTBHLoopAuton() {
     while ( true ) {
         // Get the current RPM of the flywheel
         current_fw_rpm = flywheel.get_actual_velocity();
@@ -63,17 +33,53 @@ void flywheelTBHLoopAuton() {
         }
         
         // Set the flywheel motor speeds based on the fw_output voltage
-        flywheel.move_velocity(fw_output);
+        if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) ) {
+            flywheel.move_velocity(fw_output);
+        }
+        else if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2) ) {
+            flywheel.move_velocity(-1 * fw_output);
+        }
+        else {
+            current_fw_rpm = flywheel.get_actual_velocity();
+            fw_output = 0.0;
+            tbh = 0.0;
+            prev_error = 0.0;
+            flywheel.brake();
+        }
+        pros::lcd::set_text(1, std::to_string(current_fw_rpm));
+        pros::delay(10);
     }
 }
 
+void flywheelTBHLoopAuton() {
+    // Get the current RPM of the flywheel
+    current_fw_rpm = flywheel.get_actual_velocity();
+    
+    // Calculate the error between the current RPM and the target RPM
+    error = target_fw_rpm - current_fw_rpm;
+
+    fw_output += gain * error;
+
+    if ( (error > 0 && prev_error < 0) || (error < 0 && prev_error > 0) ) {
+        fw_output = 0.5 * (fw_output + tbh);
+        tbh = fw_output;
+        prev_error = error;
+    }
+    
+    // Set the flywheel motor speeds based on the fw_output voltage
+    flywheel.move_velocity(fw_output);
+}
+
 void resetFlywheelTBH() {
-    if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y) ) {
-        flywheel.brake();
-        error = 0.0;
-        prev_error = 0.0;
-        fw_output = 0.0;
-        tbh = 0.0;
+    while ( true ) {
+        if ( controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y) ) {
+            flywheel.brake();
+            error = 0.0;
+            prev_error = 0.0;
+            fw_output = 0.0;
+            tbh = 0.0;
+        }
+        pros::delay(10);
     }
 }
 
--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -6,5 +6,6 @@
 void setIntake() {
     while ( true ) {
         intake = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
+        pros::delay(10);
     }
 }
