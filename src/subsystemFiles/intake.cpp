c7b9bd5eb4e0d191116eab68e6eaace57f090c73
9909Y-Robotics <24battistar@s.mpsct.org>
2024-01-16 21:50:22 -0500

    Reoptimize intake because of faulty motor

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -5,14 +5,6 @@
 
 void setIntake() {
     while ( true ) {
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-            intake = 127.0;
-        }
-        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-            intake = -127.0;
-        }
-        else {
-            intake = 0;
-        }
+        intake = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
     }
 }
