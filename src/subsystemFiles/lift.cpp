6c484f7c55e75ed3526928c73659598df833c32b
Ryan Battista <109159176+battistary@users.noreply.github.com>
2024-06-26 11:22:27 -0400

    Update lift.cpp

--- src/subsystemFiles/lift.cpp
+++ src/subsystemFiles/lift.cpp
@@ -5,14 +5,8 @@ bool liftRaised = false;
 void setLift() {
     while ( true ) {
         if ( controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y) ) {
-            if ( liftRaised == false ) {
-                lift.set_value(1);
-                liftRaised = true;
-            }
-            else {
-                lift.set_value(0);
-                liftRaised = false;
-            }
+            lift.set_value(!liftRaised);
+            liftRaised = !liftRaised;
         }
         pros::delay(250);
     }
